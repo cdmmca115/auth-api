bd2ffc120b9556fe8a14edec9c6be5eec3b54db2
cdmmca115 <cdmmca115@gmail.com>
2026-05-02 01:10:14 +0530

    fix duplicate db declaration

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 const app = express();
 
 // ✅ Middlewares FIRST
